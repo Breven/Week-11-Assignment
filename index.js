ed95b8f524b455545c201f040acbeeb9072c3cc6
Unknown <bh0552038@otc.edu>
2017-11-06 13:12:57 -0600

    Added more messages

--- index.js
+++ index.js
@@ -6,3 +6,6 @@ var data = jc.read('data.json');
 _.forEach(data.items, function(value){
   console.log(value);
 });
+
+console.log(`${data.items[0].fname} ${data.items[0].lname} is ${data.items[0].age} years old`);
+console.log(`${data.items[1].fname} ${data.items[1].lname} is ${data.items[1].age} years old`);
